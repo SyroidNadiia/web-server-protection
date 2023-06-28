f733eaa9a9e0d2d0d19a4679bec4b30705982f36
SyroidNadiia <112964918+SyroidNadiia@users.noreply.github.com>
2023-06-28 09:55:56 +0300

    delete mailer

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -4,13 +4,12 @@ const jwt = require("jsonwebtoken");
 const { User } = require("../../models/user");
 const { increaseLoginAttempts } = require("../../utils/authUtils");
 const dotenv = require("dotenv");
-const sendEmail = require("../../helpers/sendEmail");
 dotenv.config();
 
 const { SECRET_KEY } = process.env;
 
 const login = async (req, res) => {
-  const { email, password, verificationToken } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
   const passwordCompare = await bcrypt.compare(password, user.password);
 
@@ -19,11 +18,9 @@ const login = async (req, res) => {
     throw createError(401, `Email or password are wrong`);
   }
 
-  if (user.verify) {
-    await sendEmail(email, verificationToken);
-  } else {
-    throw createError(401, `Letter of verify does not send`);
-  }
+  // if (!user.verify) {
+  //   throw createError(401, `Letter of verify does not send`);
+  // }
 
   const payload = { id: user._id };
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
