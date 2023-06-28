591a2a0098d4064ec08ed2b0d87c2634a45038df
SyroidNadiia <112964918+SyroidNadiia@users.noreply.github.com>
2023-06-28 08:54:16 +0300

    fixed mailer

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,32 +1,44 @@
 const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.ukr.net",
-  port: 465,
-  secure: true,
-  auth: {
-    user: "shcherbelova@ukr.net",
-    pass: "22965ira",
+const transporter = nodemailer.createTransport(
+  {
+    host: "smtp.ukr.net",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "shcherbelova@ukr.net",
+      pass: "22965ira",
+    },
   },
-});
+  {
+    from: "Mailer Verify <shcherbelova@ukr.net>",
+  }
+);
 
-const { BASE_URL } = process.env;
-
-const sendEmail = async (email, verificationToken) => {
-  const mailOptions = {
-    from: "shcherbelova@ukr.net",
-    to: email,
-    subject: "Verify your email",
-    html: `<p>Click verify email ${BASE_URL}/api/auth/verify/${verificationToken}</p>`,
-  };
-
-  transporter.sendMail(mailOptions, (err, info) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Email sent: " + info.response);
-    }
+const mailer = (message) => {
+  transporter.sendMail(message, (err, info) => {
+    if (err) return console.log(err);
+    console.log("Email sent: " + info);
   });
 };
 
-module.exports = sendEmail;
+// const { BASE_URL } = process.env;
+
+// const sendEmail = async (email, verificationToken) => {
+//   const mailOptions = {
+//     from: "shcherbelova@ukr.net",
+//     to: email,
+//     subject: "Verify your email",
+//     html: `<p>Click verify email ${BASE_URL}/api/auth/verify/${verificationToken}</p>`,
+//   };
+
+//   transporter.sendMail(mailOptions, (err, info) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("Email sent: " + info.response);
+//     }
+//   });
+// };
+
+module.exports = mailer;
